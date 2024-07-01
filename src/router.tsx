a035a3e1817373a0fe17a99e5d2b5768a26d21df
Gaurav <program.drgk@gmail.com>
2024-07-01 05:08:05 -0700

    Routes modified for client side

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import PlatformPage from "./pages/PlatformPage/PlatformPage";
+import Disney from "./pages/Disney/Disney";
+import Netflix from "./pages/Netflix/Netflix";
+import PrimeVideo from "./pages/PrimeVideo/PrimeVideo";
 
 function CustomRouter() {
   return (
@@ -8,6 +11,9 @@ function CustomRouter() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/platform" element={<PlatformPage />} />
+        <Route path="/disney" element={<Disney />} />
+        <Route path="/netflix" element={<Netflix />} />
+        <Route path="/primevideo" element={<PrimeVideo />} />
       </Routes>
     </>
   );
