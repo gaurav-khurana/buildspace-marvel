7c56a86e3faf5d4ae540dd7f877a6ca54cf3e151
Gaurav <program.drgk@gmail.com>
2024-07-14 22:48:01 -0700

    Marvel card interface for props ADDED

--- src/components/MarvelCard/MarvelCard.tsx
+++ src/components/MarvelCard/MarvelCard.tsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import { MarvelCardInterface, ShowInterface } from "../../interfaces/interface";
+// import { MarvelCardInterface } from "../../interfaces/interface";
 
-function MarvelCard({ website, img, title, year, platform, description }) {
+function MarvelCard({
+  website,
+  image,
+  title,
+  year,
+  platform,
+  description,
+}: MarvelCardInterface) {
   return (
     <>
       <Link to={website}>
         <article className="shows__card">
-          <img className="shows__img" src={img} alt={title} />
+          <img className="shows__img" src={image} alt={title} />
           <h2 className="shows__title">{title}</h2>
           <h3 className="shows__year">{year}</h3>
           <h3 className="shows__platform">{platform}</h3>
