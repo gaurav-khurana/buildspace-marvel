7ad0855f68379cf09301fb403a5de596f8ce1494
Gaurav <program.drgk@gmail.com>
2024-07-25 22:57:20 -0700

    Back button for platform pages ADDED

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import "./Button.scss";
 function Button() {
   return (
     <>
-      <button className="btn--back" type="button">
+      <button className="btn btn__back" type="button">
         Back to Platform
       </button>
     </>
